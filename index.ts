b67b2a1e4e9f25f2dd4dd17517f34f5cbf89c6e3
rephaelkakoon1995 <rephaelkakoon1995@gmail.com>
2020-04-05 12:49:04 +0300

    feat(test):test

--- index.ts
+++ index.ts
@@ -14,15 +14,15 @@ app.get('/', (req, res) => {
     res.send('Welcome to the club')
 });
 
-app.get('/getAllUsersInfo', (req:express.Request, res:express.Response) => {
-    getAllUsersInfo(req,res);
-});
+// app.get('/getAllUsersInfo', (req:express.Request, res:express.Response) => {
+//     getAllUsersInfo(req,res);
+// });
 
-app.post('/addNewUser', (req:express.Request,res:express.Response) => {
-    const newUser = req.body.newUser ? req.body.newUser : "";
+// app.post('/addNewUser', (req:express.Request,res:express.Response) => {
+//     const newUser = req.body.newUser ? req.body.newUser : "";
 
-    addNewUser(req,res,newUser);
-});
+//     addNewUser(req,res,newUser);
+// });
 
 app.get('/nextVolunteer', (req:express.Request,res:express.Response) => {
     res.send('רפאל');
